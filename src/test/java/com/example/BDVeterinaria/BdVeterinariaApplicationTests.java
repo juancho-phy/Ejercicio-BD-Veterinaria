bb3c9ba8e2a49f552a1852b75b7a44861c907165
JuanC <jdiegocifuentes@ucundinamarca.edu.co>
2026-09-02 16:16:57 -0500

    inicial commit

---
+++ src/test/java/com/example/BDVeterinaria/BdVeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BDVeterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdVeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
